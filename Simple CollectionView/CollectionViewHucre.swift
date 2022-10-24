071c9f1b02f2cee432dbab53167d23dcc328c59f
ertugrullberber <ertugrulberber3461@gmail.com>
2022-10-24 11:40:34 +0300

    first commit

---
+++ Simple CollectionView/CollectionViewHucre.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewHucre.swift
+//  Simple CollectionView
+//
+//  Created by Ertugrul Berber on 23.10.2022.
+//
+
+import UIKit
+
+class CollectionViewHucre: UICollectionViewCell {
+    
+    @IBOutlet weak var hucreLabel: UILabel!
+}
